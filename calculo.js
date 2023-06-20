84b2dc06323c7be3d41c29f5e5f9583cdcb04069
Guilherme Devito <117872245+Devito552@users.noreply.github.com>
2023-06-19 21:43:38 -0300

    Add files via upload

--- calculo.js
+++ calculo.js
@@ -1,4 +1,4 @@
-function calcular(){
+function triangulo(){
     var base = prompt("Qual é a base do triangulo?");
     var altura = prompt("Qual é a altura do triangulo?");
 
@@ -6,4 +6,54 @@ function calcular(){
 
     document.getElementById("area").innerHTML = 
     "Área do triangulo é " + area + "!";
+}
+
+function circulo(){
+    var raio = prompt("Qual é o raio do circulo?");
+    var pi = 3.14;
+    var areacirculo = pi*(raio*raio);
+
+    document.getElementById("area").innerHTML = 
+    "Área do circulo é " + areacirculo + "!";
+}
+
+function quadrado(){
+    var lado_1 = prompt("Qual é o lado 1 do Quadrado?");
+    var lado_2 = prompt("Qual é o lado 2 do Quadrado?");
+
+    var areaquadrado = lado_1*lado_2;
+
+    document.getElementById("area").innerHTML = 
+    "Área do quadrado é " + areaquadrado + "!";
+}
+
+function trapezio(){
+    var basemaior = prompt("Qual é a base maior do trapezio?");
+    var basemenor = prompt("Qual é a base menor do trapezio?");
+
+    var areatrapezio = (basemaior+basemenor)/2;
+
+    document.getElementById("area").innerHTML = 
+    "Área do trapezio é " + areatrapezio + "!";
+}
+
+
+function pentagono(){
+    var pentaraio = prompt("Qual é o raio do pentagono?");
+    var pentalado = prompt("Qual é o lado do pentagono?");
+
+    var areapenta = (pentalado*5)*pentaraio;
+
+    document.getElementById("area").innerHTML = 
+    "Área do pentagono é " + areapenta + "!";
+}
+
+function losango(){
+    var losangoladomaior = prompt("Qual é a lado maior do losango?");
+    var losangoladomenor = prompt("Qual é a lado menor do losango?");
+
+    var arealosango = (losangoladomenor*losangoladomaior)/2;
+
+    document.getElementById("area").innerHTML = 
+    "Área do losango é " + arealosango + "!";
 }
